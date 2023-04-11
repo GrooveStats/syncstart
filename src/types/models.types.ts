2f47d5793c75caaf5833dc9bcfee1f78d838942f
teejusb <5017202+teejusb@users.noreply.github.com>
2023-04-11 11:55:14 -0700

    Use socket 'rooms' + add spectateLobby

--- src/types/models.types.ts
+++ src/types/models.types.ts
@@ -1,3 +1,5 @@
+import { SocketId } from 'socket.io-adapter';
+
 export class Judgments {
   fantasticPlus: number;
   fantastics: number;
@@ -19,9 +21,8 @@ export class Player {
   playerId: string;
   profileName: string;
 
+  // Provided by each player.
   judgments?: Judgments;
-
-  // Scores are recalculated as the the judgments come in.
   score?: number;
   exScore?: number;
 }
@@ -30,16 +31,46 @@ export class Spectator {
   profileName?: string;
 }
 
+export class SongInfo {
+  // The path for the song on a player's filesystem.
+  // We'll use this as a key for other players to play.
+  // e.g. 5guys1pack/Earthquake
+  //
+  // NOTE(teejusb): This requires all connected players to have those packs on
+  // their machine.
+  //
+  // NOTE(teejusb): We want to allow players to play the same *song* even if
+  // they may be different difficulties. Using a chartHash or similar is not
+  // sufficient in that case. Additionally, StepMania doesn't currently have a
+  // way to jump to a specific song based only on it's chart (as there may be
+  // conflicts). As a result, players that split up/rename the pack will not be
+  // able easily play with players that don't as the songPaths will be
+  // different.
+  songPath: string;
+  title: string;
+  artist: string;
+  stepartist: string;
+  songLength: number;
+}
+
 export class Lobby {
   code: string;
   // Empty string here is equivalent to "no password".
   password: string;
   players: Record<string, Player>;
   spectators: Spectator[];
+
+  songInfo?: SongInfo;
+
+  // All connected socketIds. This will include all the socketIds for both
+  // players and spectators. Note that more than one player may be connected for
+  // each client.
+  connectedSocketIds: Set<SocketId>;
 }
 
 export class LobbyInfo {
   code: string;
+  isPasswordProtected: boolean;
   playerCount: number;
 }
 
