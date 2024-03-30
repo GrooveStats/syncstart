fdfd6d09754f91421c7bfeec1eb9fcf1dce8d39e
teejusb <5017202+teejusb@users.noreply.github.com>
2024-03-30 12:49:51 -0700

    Changes

--- src/events/utils.ts
+++ src/events/utils.ts
@@ -8,24 +8,23 @@ import { LOBBYMAN, Lobby } from '../types/models.types';
  * @returns True if the lobby exists and the password is correct, false
  *          otherwise.
  */
-export function CanJoinLobby(code: string, password: string) {
+export function canJoinLobby(code: string, password: string) {
   // Does the lobby we're trying to join exist?
-  if (code in LOBBYMAN.lobbies) {
-    const lobby = LOBBYMAN.lobbies[code];
-    // Join either if the lobby is public, or one has provided a valid
-    // password for a private lobby.
-    if (!lobby.password || lobby.password === password) {
-      return true;
-    }
+  const lobby = LOBBYMAN.lobbies[code];
+  if (lobby === undefined) { return false; }
+
+  // Join either if the lobby is public, or one has provided a valid
+  // password for a private lobby.
+  if (!lobby.password || lobby.password === password) {
+    return true;
   }
-  return false;
 }
 
 /**
  * Generates a random lobby code.
  * @returns A random lobby code of 4 uppercase characters.
  */
-export function GenerateLobbyCode(): string {
+export function generateLobbyCode(): string {
   const lobbyCodeLength = 4;
   const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
   let result = '';
@@ -40,7 +39,7 @@ export function GenerateLobbyCode(): string {
  * @param lobby, The lobby to get the player count for.
  * @returns The number of players in the lobby.
  */
-export function GetPlayerCountForLobby(lobby: Lobby): number {
+export function getPlayerCountForLobby(lobby: Lobby): number {
   let playerCount = 0;
   for (const machine of Object.values(lobby.machines)) {
     if (machine.player1 !== undefined) {
@@ -59,42 +58,41 @@ export function GetPlayerCountForLobby(lobby: Lobby): number {
  * @param socketId, The socket ID of the machine to disconnect.
  * @returns True if the machine left the lobby, false otherwise.
  */
-export function DisconnectMachine(socketId: SocketId): boolean {
+export function disconnectMachine(socketId: SocketId): boolean {
   const code = LOBBYMAN.machineConnections[socketId];
-  if (code) {
-    const lobby = LOBBYMAN.lobbies[code];
-    if (lobby) {
-      const machine = lobby.machines[socketId];
-      if (machine) {
-        if (machine.socket) {
-          if (machine.socket.id in LOBBYMAN.machineConnections) {
-            delete LOBBYMAN.machineConnections[machine.socket.id];
-          }
+  if (code === undefined) { return false; }
 
-          machine.socket.leave(code);
-          // Don't disconnect here, as we may be re-using the connection.
-          // In the case of `leaveLobby`, the client can manually disconnect.
-        }
-        delete lobby.machines[socketId];
-        delete LOBBYMAN.machineConnections[socketId];
+  const lobby = LOBBYMAN.lobbies[code];
+  if (lobby === undefined) { return false; }
 
-        if (GetPlayerCountForLobby(lobby) === 0) {
-          for (const spectator of Object.values(lobby.spectators)) {
-            if (spectator.socket) {
-              spectator.socket.leave(code);
-              // Force a disconnect. If there are no more players in the lobby,
-              // we should remove the spectators as well.
-              spectator.socket.disconnect();
-              delete LOBBYMAN.spectatorConnections[spectator.socket.id];
-            }
-          }
-          delete LOBBYMAN.lobbies[code];
-        }
-        return true;
+  const machine = lobby.machines[socketId];
+  if (machine === undefined) { return false; }
+
+  if (machine.socket) {
+    if (machine.socket.id in LOBBYMAN.machineConnections) {
+      delete LOBBYMAN.machineConnections[machine.socket.id];
+    }
+
+    machine.socket.leave(code);
+    // Don't disconnect here, as we may be re-using the connection.
+    // In the case of `leaveLobby`, the client can manually disconnect.
+  }
+  delete lobby.machines[socketId];
+  delete LOBBYMAN.machineConnections[socketId];
+
+  if (getPlayerCountForLobby(lobby) === 0) {
+    for (const spectator of Object.values(lobby.spectators)) {
+      if (spectator.socket) {
+        spectator.socket.leave(code);
+        // Force a disconnect. If there are no more players in the lobby,
+        // we should remove the spectators as well.
+        spectator.socket.disconnect();
+        delete LOBBYMAN.spectatorConnections[spectator.socket.id];
       }
     }
+    delete LOBBYMAN.lobbies[code];
   }
-  return false;
+  return true;
 }
 
 /**
@@ -102,22 +100,31 @@ export function DisconnectMachine(socketId: SocketId): boolean {
  * @param socketId, The socket ID of the spectator to disconnect.
  * @returns, True if the spectator left the lobby, false otherwise.
  */
-export function DisconnectSpectator(socketId: SocketId): boolean {
+export function disconnectSpectator(socketId: SocketId): boolean {
   const code = LOBBYMAN.spectatorConnections[socketId];
-  if (code) {
-    const lobby = LOBBYMAN.lobbies[code];
-    if (lobby) {
-      const spectator = lobby.spectators[socketId];
-      if (spectator) {
-        if (spectator.socket) {
-          spectator.socket.leave(code);
-          // Don't disconnect here, as we may be re-using the connection.
-        }
-        delete lobby.spectators[socketId];
-        delete LOBBYMAN.spectatorConnections[socketId];
-        return true;
-      }
-    }
+  if (code === undefined) { return false; }
+
+  const lobby = LOBBYMAN.lobbies[code];
+  if (lobby === undefined) { return false; }
+
+  const spectator = lobby.spectators[socketId];
+  if (spectator === undefined) { return false; }
+
+  if (spectator.socket) {
+    spectator.socket.leave(code);
+    // Don't disconnect here, as we may be re-using the connection.
   }
-  return false;
+  delete lobby.spectators[socketId];
+  delete LOBBYMAN.spectatorConnections[socketId];
+  return true;
+}
+
+/** Gets the lobby for specific connection.
+ * @returns, lobby if the machine is part of one, or undefined otherwise.
+ */
+export function getLobbyForMachine(socketId: SocketId): Lobby | undefined {
+  const code = LOBBYMAN.machineConnections[socketId];
+  if (code === undefined) { return undefined };
+
+  return LOBBYMAN.lobbies[code];
 }
